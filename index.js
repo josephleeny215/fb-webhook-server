21ad6303523bf0b455c77314c1d9116832d5a991
josephleeny215 <josephleeny215@gmail.com>
2025-06-29 15:44:33 -0400

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const VERIFY_TOKEN = process.env.VERIFY_TOKEN || 'sigmaHippo';
 app.use(bodyParser.json());
 
 // Facebook webhook verification
-app.get('webhook', (req, res) = {
+app.get('/webhook', (req, res) => {
   const mode = req.query['hub.mode'];
   const token = req.query['hub.verify_token'];
   const challenge = req.query['hub.challenge'];
