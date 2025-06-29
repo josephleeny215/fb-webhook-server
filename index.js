b3f814abb7a43f104a6b929fd99fe2fabfde49fc
josephleeny215 <josephleeny215@gmail.com>
2025-06-29 15:50:27 -0400

    Update index.js

--- index.js
+++ index.js
@@ -25,5 +25,5 @@ app.post('/webhook', (req, res) => {
   res.sendStatus(200);
 });
 
-const PORT = process.env.PORT  3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () = console.log(`Webhook server listening on port ${PORT}`));
